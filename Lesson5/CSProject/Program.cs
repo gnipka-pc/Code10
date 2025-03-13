f20029db60946e2b2f472caa6700e50420cfd880
brennelul <brennelul@gmail.com>
2025-03-13 22:37:03 +0700

    Практика lesson5

--- Lesson5/CSProject/Program.cs
+++ Lesson5/CSProject/Program.cs
@@ -5,11 +5,12 @@ class Car
     // Поля (свойства машины)
     public string Brand;
     public string Model;
-    public int Year;
+    protected int Year;
 
     // Конструктор (вызывается при создании объекта)
     public Car(string brand, string model, int year)
     {
+        if (year < 0) return;
         Brand = brand;
         Model = model;
         Year = year;
@@ -20,14 +21,51 @@ class Car
     {
         Console.WriteLine($"Авто: {Brand} {Model}, {Year} года");
     }
+
+    public void SetYear(int year)
+    {
+        if (year < 0) return;
+        Year = year;
+    }
 }
+
 
 // Использование класса
 class Program
 {
     static void Main()
-    {
+    {   
+
         Car myCar = new Car("Toyota", "Corolla", 2022); // Создаём объект
         myCar.ShowInfo(); // Выведет: "Авто: Toyota Corolla, 2022 года"
+        myCar.Year = 0;
+
+        // int[,] matrix = {
+        //     { 1, 2, 3 },
+        //     { 4, 5, 6 },
+        //     { 7, 8, 9 }
+        // };
+
+        // int[,] matrix_2 = matrix;
+
+        // matrix_2[2, 2] = 99;
+
+        // for (int i = 0; i < matrix.GetLength(0); i++)
+        // {
+        //     for (int j = 0; j < matrix.GetLength(1); j++)
+        //     {
+        //         System.Console.Write(matrix[i, j] + " ");
+        //     }
+        //     System.Console.WriteLine();
+        // }
+
+        // foreach (int num in matrix)
+        // {
+        //     Console.WriteLine(num);
+        // }
+
+        // 1 2 3
+        // 4 5 6
+        // 7 8 9
     }
 }
--- Lesson5/Practice/Program.cs
+++ Lesson5/Practice/Program.cs
@@ -1,1 +1,25 @@
 ﻿using System;
+
+class Person
+{
+    public string name;
+    public int age;
+
+    public void Introduce()
+    {
+        System.Console.WriteLine($"Привет, мое имя {name}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.name = "Ivan";
+        person.age = 50;
+
+        System.Console.WriteLine(person.name);
+        System.Console.WriteLine(person.age);
+    }
+}
