758daba4e072d82390c80f45b49154a6018abedf
brennelul <brennelul@gmail.com>
2025-03-17 22:35:54 +0700

    Добавлены материалы с урока 6

--- Lesson6/TestFile/Program.cs
+++ Lesson6/TestFile/Program.cs
@@ -3,14 +3,42 @@ using System.IO;
 
 class Program
 {
+    static void addArrayToFile(string path, int[] array){
+        var file = File.AppendText(path);
+        foreach (int item in array)
+        {
+            file.Write($"{item} ");
+        }
+        file.Close();
+    }
+
     static void Main()
     {
         const string path = "example.txt";
+
+        // int[] array = { 50, 40, 30, 20 };
+
+        // addArrayToFile(path, array);
+
+
+        // string[] file = { "Еще одна строка", "abc" };
+
+        // File.AppendAllLines(path, file);
+        // File.WriteAllLines(path, file);
+
+
+        // foreach (string item in file)
+        // {
+        //     System.Console.WriteLine(item);
+        // }
+        
+        // file[0] = "Новый текст";
+
         string content = "Привет, мир!";
 
         File.WriteAllText(path, content);
 
-        if (File.Exists("example.txt")){
+        if (File.Exists(path)){
             Console.WriteLine("Файл существует!");
             
             string text = File.ReadAllText(path);
@@ -23,5 +51,8 @@ class Program
                 Console.WriteLine(line);
             }
         }
+
+        // int[] newArray = { 1, 2, 3, 4, 5 };
+        // addArrayToFile(path, newArray);
     }
 }
