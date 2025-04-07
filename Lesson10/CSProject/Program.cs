54823b0ac261857161cc62ef2e23c69012fe8a10
brennelul <brennelul@gmail.com>
2025-04-07 23:02:13 +0700

    Материалы и код к уроку 10

---
+++ Lesson10/CSProject/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+class FileManager
+{
+    private string _currentDirectory;
+    private string[] _pages;
+    public FileManager()
+    {
+        _currentDirectory = Directory.GetCurrentDirectory();
+    }
+
+    public void printDirectory()
+    {
+        System.Console.WriteLine($"Текущая директория: {_currentDirectory}");
+        string[] dirFiles = Directory.GetFiles(_currentDirectory);
+        string[] dirDirectories = Directory.GetDirectories(_currentDirectory);
+        int i = 1;
+        foreach(string dir in dirDirectories)
+        {
+            System.Console.WriteLine($"{i}. {dir}");
+            i++;
+        }
+        foreach(string file in dirFiles)
+        {
+            System.Console.WriteLine($"{i}. {file}");
+            i++;
+        }
+    }
+    
+    public void changeDirectory(string newDirectory)
+    {
+        if (Directory.Exists(newDirectory))
+        {
+            _currentDirectory = newDirectory;
+        }
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        FileManager fm = new FileManager();
+        string input = "";
+        fm.printDirectory();
+        while (true)
+        {
+            string[] temp = input.Split(' ');
+            if (temp[0] == "cd")
+            {
+                fm.changeDirectory(temp[1]);
+            }
+            input = System.Console.ReadLine();
+            fm.printDirectory();
+        }
+    }
+}
+        
