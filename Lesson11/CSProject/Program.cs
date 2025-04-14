c941b9a5bb70c811e063e0c33dc717a5d1ce9fb9
brennelul <brennelul@gmail.com>
2025-04-14 22:35:28 +0700

    Практика на уроке 11

--- Lesson11/CSProject/Program.cs
+++ Lesson11/CSProject/Program.cs
@@ -1,8 +1,74 @@
-﻿class Program
+﻿using System.Runtime.InteropServices.Swift;
+using System.Security.Cryptography;
+
+// public delegate void MessageHandler();
+
+// public class Publisher
+// {
+//     public event MessageHandler MessageSent;
+
+//     public void SendMessage()
+//     {
+//         System.Console.WriteLine("Сообщение отправлено");
+//         MessageSent?.Invoke();
+//     }
+// }
+
+// public class Subscriber
+// {
+//     public void OnMessage()
+//     {
+//         System.Console.WriteLine("Подписчик получил сообщение");
+//     }
+// }
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Publisher publisher = new Publisher();
+//         Subscriber subscriber = new Subscriber();
+
+//         publisher.MessageSent += subscriber.OnMessage; // подписка на событие
+
+//         publisher.SendMessage();
+//     }
+// }
+     
+
+public delegate void ClickHandler();
+
+public class Button
+{
+    public ClickHandler OnClick;
+
+    public void Click()
+    {
+        System.Console.WriteLine("Кнопка нажата");
+        OnClick?.Invoke();
+    }
+}
+
+public class Window
+{
+    public void ShowWindow()
+    {
+        System.Console.WriteLine("|-------|");
+        System.Console.WriteLine("|   OK  |");
+        System.Console.WriteLine("|       |");
+        System.Console.WriteLine("|-------|");
+    }
+}
+
+class Program
 {
     static void Main()
     {
-        
+        Button button = new Button();
+        Window window = new Window();
+
+        button.OnClick += window.ShowWindow;
+
+        button.Click();
     }
 }
-     
---
+++ Lesson11/Practice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+public delegate double MathOperation(double x, double y);
+
+class Program
+{
+    public static double Add(double x, double y)
+    {
+        return x + y;
+    }
+
+    public static double Multiply(double x, double y)
+    {
+        return x * y;
+    }
+
+    public static double ExecuteOperation(MathOperation operation, double x, double y)
+    {
+        return operation(x, y);
+    }
+
+    public static void Main()
+    {
+        MathOperation handler = Multiply;
+        double i = ExecuteOperation(handler, 5, 5);
+        Console.WriteLine(i);
+        handler = Add;
+        i = ExecuteOperation(handler, 5, 5);
+        Console.WriteLine(i);
+    }
+}
