cf4db8314f4949c408a50a44f9a97ff107f75c1a
brennelul <brennelul@gmail.com>
2025-04-21 22:34:19 +0700

    Задания для 13 урока

---
+++ Lesson13/CSProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        using HttpClient client = new HttpClient();
+        string url = "https://jsonplaceholder.org/posts/1";
+        string response = await client.GetStringAsync(url);
+
+        System.Console.WriteLine(response);
+    }
+}
