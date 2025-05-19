41ffa1da8a905f25edf6ea762925c56de0810071
brennelul <brennelul@gmail.com>
2025-05-19 23:04:36 +0700

    практика урок 18

---
+++ Lesson18/EasyTrackerAPI/Model/Data/AccountContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AccountContext : DbContext
+{
+    public AccountContext(DbContextOptions<AccountContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasKey(user => user.ID);
+    }
+}
+
+public class Responce
+{
+    public User user = new User("1");
+    public bool IsAutorized;
+}
